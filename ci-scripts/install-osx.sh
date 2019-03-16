7794734dd1aa6b895599a781cc04005911c72d24
Olemis Lang <olemis@simelo.tech>
2019-03-15 21:19:05 -0400

    [ci] refs #36 - Run skycoin/hardware-wallet emulator on Travis. Install deps

--- ci-scripts/install-osx.sh
+++ ci-scripts/install-osx.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+set -x
+
+brew update
+# USB HID enumeration libraries
 brew install libusb
+# SDL for building hardware wallet emulator
+brew install sdl2_image sdl2 mesa mesalib-glw
 
