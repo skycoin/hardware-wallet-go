8cb2d8b13e7edbbe571d65c961a448bfac2f6eda
Alvaro Denis <dev0003@simelo.tech>
2019-04-05 16:42:06 -0400

    define device as user wants ref #86

--- src/cli/get_entropy.go
+++ src/cli/get_entropy.go
@@ -27,7 +27,7 @@ func getEntropyCmd() gcli.Command {
 				log.Error("outFile is mandatory")
 				return
 			}
-			device := deviceWallet.NewDevice(deviceWallet.DeviceTypeUSB)
+			device := deviceWallet.NewDevice(deviceWallet.DeviceTypeFromString(c.String("deviceType")))
 			if device == nil {
 				return
 			}
@@ -48,6 +48,11 @@ func getEntropyCmd() gcli.Command {
 			gcli.StringFlag{
 				Name:  "outFile",
 				Usage: "File path to write out the entropy buffers.",
+			},
+			gcli.StringFlag{
+				Name:   "deviceType",
+				Usage:  "Device type to send instructions to, hardware wallet (USB) or emulator.",
+				EnvVar: "DEVICE_TYPE",
 			},
 		},
 		SkipFlagParsing:    false,
