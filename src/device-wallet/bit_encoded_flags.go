614a3d6e3fad2c633ae3c20043a3e6cee0990fc0
Alvaro Denis <dev0003@simelo.tech>
2019-05-03 13:56:00 -0400

    add useful functions to handle independent bits in integers ref #117

--- src/device-wallet/bit_encoded_flags.go
+++ src/device-wallet/bit_encoded_flags.go
@@ -4,3 +4,16 @@ type BitEncodedFlags interface {
 	Marshal(v interface{}) (uint64, error)
 	Unmarshal() error
 }
+
+func bitStatusInByte(data, bitPos uint8) bool {
+	return (data & (uint8)(1 << bitPos)) != 0
+}
+
+func setBitInByte(data *uint8, val bool, bitPos uint8) {
+	mask := (uint8)(1 << bitPos)
+	if val {
+		*data |= mask
+	} else {
+		*data &= mask ^ 255
+	}
+}
