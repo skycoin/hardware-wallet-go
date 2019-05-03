87a519a411bc6e8c6e1b695885704f1ae4be0830
Alvaro Denis <dev0003@simelo.tech>
2019-05-03 13:59:42 -0400

    Add FirmwareFeatures as an inmpmenetation of BitEncodedFlags ref #117

--- src/device-wallet/bit_encoded_flags.go
+++ src/device-wallet/bit_encoded_flags.go
@@ -1,8 +1,48 @@
 package devicewallet
 
+import (
+	"encoding/binary"
+	"encoding/json"
+)
+
 type BitEncodedFlags interface {
 	Marshal(v interface{}) (uint64, error)
 	Unmarshal() error
+}
+
+type FirmwareFeatures struct {
+	flags uint64
+	RequireGetEntropyConfirm bool
+	IsGetEntropyEnabled bool
+	IsEmulator bool
+	FirmwareFeaturesRdpDebugDisabled uint8
+	FirmwareFeaturesRdpMemProtect uint8
+	FirmwareFeaturesRdpLevel uint8
+}
+
+func NewFirmwareFeatures(flags uint64) *FirmwareFeatures {
+	return &FirmwareFeatures{flags: flags}
+}
+
+func (ff FirmwareFeatures) Marshal(v interface{}) (uint64, error) {
+	bs := make([]byte, 8)
+	setBitInByte(&bs[7], ff.RequireGetEntropyConfirm, 0)
+	setBitInByte(&bs[7], ff.IsGetEntropyEnabled, 1)
+	setBitInByte(&bs[7], ff.IsEmulator, 2)
+	setBitInByte(&bs[7], ff.FirmwareFeaturesRdpLevel == 1, 3)
+	setBitInByte(&bs[7], ff.FirmwareFeaturesRdpLevel == 2, 4)
+	return ff.flags, nil
+}
+
+func (ff *FirmwareFeatures) Unmarshal() error {
+	bs := make([]byte, 8)
+	binary.BigEndian.PutUint64(bs, ff.flags)
+	ff.RequireGetEntropyConfirm = bitStatusInByte(bs[7], 0)
+	ff.IsGetEntropyEnabled = bitStatusInByte(bs[7], 1)
+	ff.IsEmulator = bitStatusInByte(bs[7], 2)
+	setBitInByte(&ff.FirmwareFeaturesRdpLevel, bitStatusInByte(bs[7], 3), 0)
+	setBitInByte(&ff.FirmwareFeaturesRdpLevel, bitStatusInByte(bs[7], 4), 1)
+	return nil
 }
 
 func bitStatusInByte(data, bitPos uint8) bool {
